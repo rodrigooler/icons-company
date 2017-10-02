70083253e65025b6ca4feb0ab730a57cdf32a317
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2017-10-02 20:24:46 -0300

    Fix package.json and scripts/icons.js

--- scripts/icons.js
+++ scripts/icons.js
@@ -4,7 +4,7 @@ const pngFolder = './png/';
 
 fs.readdir(pngFolder, (err, files) => {
   const pngList = files.forEach(file => {
-    const fileName = file;
+    const fileName = file
     return `![${fileName}](./png/${fileName})`;
   });
 
