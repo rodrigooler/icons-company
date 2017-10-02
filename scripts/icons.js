f1cf25d669c3a3777071005541ae98e212cf7d26
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2017-10-02 20:26:56 -0300

    Run prettier

--- scripts/icons.js
+++ scripts/icons.js
@@ -1,19 +1,18 @@
-import fs from 'fs'
+import fs from 'fs';
 
 const pngFolder = './png/';
 
 fs.readdir(pngFolder, (err, files) => {
   const pngList = files.forEach(file => {
-    const fileName = file
+    const fileName = file;
     return `![${fileName}](./png/${fileName})`;
   });
 
-  fs.writeFile('ICONS.md', ...pngList, (err) => {
+  fs.writeFile('ICONS.md', ...pngList, err => {
     if (err) {
       return console.log(err);
     }
 
     console.log('The file was saved!');
   });
-
 });
